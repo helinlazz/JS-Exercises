b85c33a2bac05ff7042645f857d0ea346d500cde
Mirka <mirkamedaglini@hotmail.it>
2023-06-05 11:30:25 +0200

    Exercise completed

---
+++ 10. Optional Chaining/exercise.js
@@ -0,0 +1,15 @@
+const order = {};
+/* 
+if (
+  order &&
+  order.customer &&
+  order.customer.address &&
+  !order.customer.address.city
+) {
+  console.log("City is required");
+}
+ */
+
+if (!order?.customer?.address?.city) {
+  console.log("City is required");
+}
