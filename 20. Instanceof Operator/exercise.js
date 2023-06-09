42af22c3d35d7e3455f1c2f5eaaf3d7b949cf2dc
Mirka <mirkamedaglini@hotmail.it>
2023-06-09 10:52:04 +0200

    Exercise completed

---
+++ 20. Instanceof Operator/exercise.js
@@ -0,0 +1,38 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+  static calculate(type) {
+    if (type instanceof Square) {
+      return type.side * type.side;
+    } else if (type instanceof Rectangle) {
+      return type.width * type.height;
+    } else if (type instanceof Circle) {
+      return type.radius * 2 * 3.14;
+    }
+  }
+}
+
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle));
